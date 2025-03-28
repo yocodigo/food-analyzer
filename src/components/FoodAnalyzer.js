96d0053cd6f7f44aad18ad2c9e60154024edf5b2
Joel <contact_jhern@proton.me>
2025-03-28 13:50:30 -0700

    Create FoodAnalyzer component, modify App.js

---
+++ src/components/FoodAnalyzer.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { Camera } from 'lucide-react';
+
+const FoodAnalyzerApp = () => {
+        return (
+          <div>
+            Hello World!
+          </div>
+        );
+}
+
+export default FoodAnalyzerApp;
